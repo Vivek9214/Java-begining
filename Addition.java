d3160200aa18fbbd790c875450fad84e4b6f58cc
CoderVivek92 <vivekgahlot92@gmail.com>
2024-10-19 17:01:14 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,11 @@
+public class Addition
+ {
+    public static void main(String[] args)
+    {
+        int a, b, c;
+        a = 45;
+        b = 67;
+        c = a + b;
+        System.out.println(c);
+    }    
+}
