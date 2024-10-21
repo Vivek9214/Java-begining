a91e38da6fbc7d5c7d07e4bb1f3095ab4483c9e1
CoderVivek92 <vivekgahlot92@gmail.com>
2024-10-21 21:19:18 +0530

    Add files via upload

---
+++ Datatype2.java
@@ -0,0 +1,17 @@
+public class Datatype2 {
+    public static void main(String[] args) 
+    {
+        short s = 58;
+        int i = 189;
+        long l = 37000;
+        double d = 57.987;
+        char c = 'T';
+        String st = "S.B.j.S Rampuria Jain College, Bikaner";
+        System.out.println("Short value is: " + s);
+        System.out.println("Interger value is: " + i);
+        System.out.println("Long variable is : " + l);
+        System.out.println("double variable is : " + d);
+        System.out.println("Char variable is : " + c);
+        System.out.println("String Data is : " + st);
+    }   
+}
