e703b1c134aba84753b9d1d7795057289366c1da
CoderVivek92 <vivekgahlot92@gmail.com>
2024-10-23 20:33:20 +0530

    Add files via upload

---
+++ Divide3.java
@@ -0,0 +1,9 @@
+public class Divide3 {
+    public static void main(String[] args)
+    {
+        int a = 10;
+        int b = 5;
+        int result = a/b;
+        System.out.println("Result: " + result);
+    }
+}
