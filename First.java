18d4177a10ffb50e4967be67d6d55e0b7bc8c87b
CoderVivek92 <vivekgahlot92@gmail.com>
2024-10-23 20:35:53 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,10 @@
+public class First{
+    public static void main(String[]args){
+    double a,b,c,e1;
+    a=11.0;
+    b=117.0;
+    c=10.05;
+    e1=(-b + Math.sqrt(b*b - 4.0*a*c)) / (2.0*a);
+    System.out.println(e1);
+}
+}
