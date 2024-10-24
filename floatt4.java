eb4fa12b645c67e13985e04b4684788d868a0a22
CoderVivek92 <vivekgahlot92@gmail.com>
2024-10-24 21:16:01 +0530

    Add files via upload

---
+++ floatt4.java
@@ -0,0 +1,17 @@
+public class floatt4 {
+    public static void main(String[] args)
+    {
+        double a = 12.42; 
+        double b = 13.23;
+        double add = a + b;
+        double sub = a - b;
+        double mul = a * b;
+        double div = a / b;
+
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(mul);
+        System.out.println(div);
+        
+    }
+}
