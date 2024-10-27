12cb1d724855aed782bec9d380885f999d8eec56
CoderVivek92 <vivekgahlot92@gmail.com>
2024-10-28 01:18:05 +0530

    Add files via upload

---
+++ Qr6.java
@@ -0,0 +1,11 @@
+public class Qr6 {
+    public static void main(String[] args)
+    {
+        int a=11, b=2;
+        int quotient = a/b;
+        int reminder = a % b;
+        System.out.println("Quotient is: "+quotient);
+        System.out.println("Reminder is " + reminder);
+    }
+    
+}
