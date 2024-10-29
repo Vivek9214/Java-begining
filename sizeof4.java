adc73b70f4401b72d1bac0013818183718678435
CoderVivek92 <vivekgahlot92@gmail.com>
2024-10-29 22:56:05 +0530

    Add files via upload

---
+++ sizeof4.java
@@ -0,0 +1,9 @@
+public class sizeof4 {
+    public static void main(String[] args) {
+        System.out.println("Size of int: "+ Integer.SIZE + " bits");
+        System.out.println("Size of float: " + Float.SIZE + " bits");
+        System.out.println("Size of Double: " + Double.SIZE + " bits");
+        System.out.println("Size of Char: " + Character.SIZE + " bits");
+    }
+    
+}
