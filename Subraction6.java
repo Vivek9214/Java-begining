438424e5bef6099001bd58b0ae65273a7c35c453
CoderVivek92 <vivekgahlot92@gmail.com>
2024-11-01 10:45:20 +0530

    Add files via upload

---
+++ Subraction6.java
@@ -0,0 +1,10 @@
+public class Subraction6 {
+    public static void main(String[] args)
+    {
+        int a, b, c;
+        a = 12;
+        b = 63;
+        c = b - a;
+        System.out.println("Subraction of a and b is: " + c);
+    }
+}
