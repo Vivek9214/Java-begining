97def54e50988ec09a9fd1c7c03166b188ca9195
CoderVivek92 <vivekgahlot92@gmail.com>
2024-11-03 12:52:35 +0530

    Add files via upload

---
+++ Multiply7.java
@@ -0,0 +1,9 @@
+public class Multiply7 {
+    public static void main(String[] args)
+    {
+        int num1 = 5;
+        int num2 = 6;
+        int result = num1 * num2;
+        System.out.println("The result of the multiplication is: " + result);
+    }
+}
