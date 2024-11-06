edd3b052bddfcc76d6282f43a03c862d4a6f55e3
CoderVivek92 <vivekgahlot92@gmail.com>
2024-11-06 23:06:58 +0530

    Add files via upload

---
+++ Wap2.java
@@ -0,0 +1,7 @@
+public class Wap2 {
+    public static void main(String[] args){
+        System.out.println("WELCOM");
+        System.out.println("TO");
+        System.out.println("Java");
+    }    
+}
