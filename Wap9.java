ea36613eba52b66a309fc6ee25cbc386cfbdf535
CoderVivek92 <vivekgahlot92@gmail.com>
2024-11-07 11:22:16 +0530

    Add files via upload

---
+++ Wap9.java
@@ -0,0 +1,17 @@
+public class Wap9 {
+    public static void main(String[] args)
+    {
+        int a, b,add, sub, mul,div;
+        a = 50;
+        b = 10;
+        add = a + b;
+        sub = a - b;
+        mul = a * b;
+        div = a / b;
+        
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(mul);
+        System.out.println(div);
+    }    
+}
