54cc9bd8c878573bc4aa1649d08155c1d2f98b93
CoderVivek92 <vivekgahlot92@gmail.com>
2024-11-08 21:15:44 +0530

    Add files via upload

---
+++ Wap12.java
@@ -0,0 +1,7 @@
+public class Wap12 {
+    public static void main(String[] args) {
+        double a = 106.89, b = 34.67;
+        double c = a-b;
+        System.out.println("Subtraction of a and b is: " + c);
+    } 
+}
