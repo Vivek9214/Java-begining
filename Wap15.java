69c105496ce77255b55c87cc053ef47ca51f39ad
CoderVivek92 <vivekgahlot92@gmail.com>
2024-11-09 18:20:11 +0530

    Add files via upload

---
+++ Wap15.java
@@ -0,0 +1,11 @@
+public class Wap15 {
+    public static void main(String[] args)
+    {
+        double x, a, b, c;
+        a = 15;
+        b = 5;
+        c = 10;
+        x = a+b/c;
+        System.out.println(x);
+    }
+}
