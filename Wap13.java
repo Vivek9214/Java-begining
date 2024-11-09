06b4ce96c420627309abc7d68e54c4884f84bd81
CoderVivek92 <vivekgahlot92@gmail.com>
2024-11-09 18:19:38 +0530

    Add files via upload

---
+++ Wap13.java
@@ -0,0 +1,7 @@
+public class Wap13 {
+    public static void main(String[] args){
+        double a = 45.78, b = 9.89;
+        double c= a*b; 
+        System.out.println("Multiplication of a and b is: " + c);
+    }
+}
