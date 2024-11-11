6c61f10d0374b34ab586e1f27a40669c48aca401
CoderVivek92 <vivekgahlot92@gmail.com>
2024-11-11 17:01:20 +0530

    Add files via upload

---
+++ Wap17.java
@@ -0,0 +1,12 @@
+public class Wap17 {
+    public static void main(String[] args){
+        int num1 = 34;
+        int num2 = 67;
+        int num3 = 89;
+        int num4 = 34;
+        int sum = num1 + num2 + num3 + num4;
+        System.out.println("Sum of the given numbers: " + sum);
+        int average = sum / 4;
+        System.out.println("Average of the given numbers: " + average);
+    }
+}
