fb489ff4e498f0b306b4d89d58e1eb7d507aaa56
CoderVivek92 <vivekgahlot92@gmail.com>
2024-11-14 13:47:16 +0530

    Add files via upload

---
+++ Wap19.java
@@ -0,0 +1,21 @@
+import java.lang.Math;
+public class Wap19 {
+    public static void main(String args[]){
+        double a = 5;
+        double b = 2;
+        System.out.println(Math.pow(a,b));
+        
+        double c = 5;
+        double d = 4;
+        System.out.println(Math.pow(c,d));
+        
+        double e = 5; 
+        double f = 3 + 2;
+        System.out.println(Math.pow(e,f));
+        
+        double g = 5;
+        double h = 2;
+        double i = 6;
+        System.out.println(Math.pow(g,h)+ Math.pow(g,i));
+    }
+}
