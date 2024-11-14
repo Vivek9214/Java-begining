1a64ff653988b14191fec7220e9c8b962715f9ad
CoderVivek92 <vivekgahlot92@gmail.com>
2024-11-14 13:46:57 +0530

    Add files via upload

---
+++ Wap18.java
@@ -0,0 +1,7 @@
+public class Wap18 {
+    public static void main(String[] args){
+        int p = 100, r = 6, t = 2;
+        int si = p*r*t/100;
+        System.out.println("Simple Interest: " + si);
+    }
+}
