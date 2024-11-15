044b188220074795d31e9fb09a14c65d860a98d5
CoderVivek92 <vivekgahlot92@gmail.com>
2024-11-15 21:50:16 +0530

    Add files via upload

---
+++ Wap20.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Wap20 {
+    public static void main(String[] args) {
+    Double num;
+    Scanner sc= new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    num = sc.nextDouble();
+    Double squareroot = Math.pow(num, 0.5);
+    System.out.println("The Square of a Given Number  " + num + "  =  " + squareroot);
+    sc.close();
+    }
+    }
